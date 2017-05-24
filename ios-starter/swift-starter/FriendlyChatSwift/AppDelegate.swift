358667d9cdf2a3f3fce8fbeded0acc343bdb537e
James Zhou <zhouzihan2014@gmail.com>
2017-05-23 23:11:46 -0700

    FirebaseApp.configure()

--- ios-starter/swift-starter/FriendlyChatSwift/AppDelegate.swift
+++ ios-starter/swift-starter/FriendlyChatSwift/AppDelegate.swift
@@ -44,6 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions
     launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
     GIDSignIn.sharedInstance().delegate = self
     return true
   }
